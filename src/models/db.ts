f036547e524e33f5112adbe06da962b8c3c2d398
Shreyas Bhanderi <shreyas.bhanderi@nagarro.com>
2018-05-15 00:52:41 +0530

    make it better

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 
-const db = new Sequelize('lms', 'jarvis', 'ultron', {
-    host: 'localhost',
+const db = new Sequelize('d1u8044050it0', 'jgarhrcfoklwkd', '6824b008531d48a91dc245fde414d6f38ae0eee958c3208e30e37ce8a4ecd624', {
+    host: 'ec2-50-19-224-165.compute-1.amazonaws.com',
     dialect: 'postgres',
+    port:'5432',
     pool: {
         min: 0,
         max: 5,
